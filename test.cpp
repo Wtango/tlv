9106c08954d8084b4e51b7cb5c358b3cb01fc0fa
wuyangtang <wuyangtang@live.com>
2015-12-01 21:19:09 +0800

    test program

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+
 #include "TLVPackage.h"
 
 static void PrintBufferHex(const uint8_t* buff, size_t len)
@@ -14,20 +15,6 @@ static void PrintBufferHex(const uint8_t* buff, size_t len)
 	printf("\n");
 }
 
-uint8_t data[] =
-{
-	0x70,0x43,0x5F,0x20,0x1A,0x56,0x49,0x53,
-	0x41,0x20,0x41,0x43,0x51,0x55,0x49,0x52,
-	0x45,0x52,0x20,0x54,0x45,0x53,0x54,0x20,
-	0x43,0x41,0x52,0x44,0x20,0x32,0x39,0x57,
-	0x11,0x47,0x61,0x73,0x90,0x01,0x01,0x00,
-	0x10,0xD1,0x01,0x22,0x01,0x11,0x43,0x87,
-	0x80,0x89,0x9F,0x1F,0x10,0x31,0x31,0x34,
-	0x33,0x38,0x30,0x30,0x37,0x38,0x30,0x30,
-	0x30,0x30,0x30,0x30,0x30,
-	0xff,0x11,0x03,'p','e','r'
-};
-
 uint8_t tlv1Data[] = 
 {
 	0x22,0x19,0x21,0x6,0xf,0x4,0x4,0x0,0x0,
@@ -37,7 +24,6 @@ uint8_t tlv1Data[] =
 
 int main()
 {
-#if 1
 	uint32_t tlv_size = 0;
 	Tlv_t tlvs[MAX_TLVOBJ_SIZE];
 	Tlv_t tlvs2[MAX_TLVOBJ_SIZE];
@@ -59,39 +45,10 @@ int main()
 	printf("tmp_size:%d\n",tmp_size);
 	PrintBufferHex(tmp,tmp_size);
 
-	if(!memcmp(tlv1Data,tmp,sizeof(tlv1Data))) {
+	if(!memcmp(tlv1Data + 2,tmp,sizeof(tlv1Data) - 2)) {
 		printf("the data is same..\n");
 	}
-#else
-
-	Tlv_t tlv;
-	tlv.tag = 0xa1;
-
-	Tlv_t child1;
-	child1.tag = 0x01;
-	uint8_t child1_msg[] = {'c','h','i','l','d','1'};
-	child1.length = sizeof(child1_msg);
-	TLVPackage::CopyBuff2TlvValue(child1_msg, &child1);
-
-	Tlv_t child2;
-	child2.tag = 0x02;
-	uint8_t child2_msg[] = {'c','h','i','l','d','2'};
-	child2.length = sizeof(child2_msg);
-	TLVPackage::CopyBuff2TlvValue(child2_msg, &child2);
-
-	if(TLVPackage::AddTlv(&tlv,&child1))
-		fprintf(stderr,"Add child1 error\n");
-	if(TLVPackage::AddTlv(&tlv,&child2))
-		fprintf(stderr,"Add child2 error\n");
-
-	TLVPackage::Tlv_Debug(&tlv, 1);
-
-	Tlv_t tlvs[MAX_TLVOBJ_SIZE];
-	uint32_t tlvs_size = 0;
-	TLVPackage::Construct(tlv.value, tlv.length, tlvs, tlvs_size);
-
-	TLVPackage::Tlv_Debug(tlvs, tlvs_size);
-#endif
-
-
+	else {
+		printf("the data is not same..\n");
+	}
 }
